351ccd0480d7738aa9801ce22770a4f91ae6896b
Edward Thomson <ethomson@edwardthomson.com>
2020-02-23 11:54:33 +0000

    cli: introduce a help command
    Add a framework for commands to be defined, and add our first one,
    "help".  When `git2_cli help` is run, the `cmd_help` function will be
    invoked with the remaining command line arguments.  This allows users to
    invoke `git2_cli help foo` to get information about the `foo` subcommand.

--- src/cli/main.c
+++ src/cli/main.c
@@ -8,18 +8,32 @@
 #include <stdio.h>
 #include <git2.h>
 #include "cli.h"
+#include "cmd.h"
 
+static int show_help = 0;
 static int show_version = 0;
+static char *command = NULL;
+static char **args = NULL;
 
-static const cli_opt_spec common_opts[] = {
+const cli_opt_spec cli_common_opts[] = {
+	{ CLI_OPT_SWITCH, "help",      0, &show_help,    1, NULL,      "display help information" },
 	{ CLI_OPT_SWITCH, "version",   0, &show_version, 1, NULL,      "display the version" },
+	{ CLI_OPT_ARG,    "command",   0, &command,      0, "command", "the command to run", CLI_OPT_USAGE_REQUIRED },
+	{ CLI_OPT_ARGS,   "args",      0, &args,         0, "args",    "arguments for the command" },
 	{ 0 }
+};
+
+const cli_cmd_spec cli_cmds[] = {
+	{ "help", cmd_help, "Display help information" },
+	{ NULL }
 };
 
 int main(int argc, char **argv)
 {
+	const cli_cmd_spec *cmd;
 	cli_opt_parser optparser;
 	cli_opt opt;
+	int args_len = 0;
 	int error = 0;
 
 	if (git_libgit2_init() < 0) {
@@ -27,15 +41,25 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	cli_opt_parser_init(&optparser, common_opts, argv + 1, argc - 1);
+	cli_opt_parser_init(&optparser, cli_common_opts, argv + 1, argc - 1);
 
 	/* Parse the top-level (common) options and command information */
 	while (cli_opt_parser_next(&opt, &optparser)) {
 		if (!opt.spec) {
 			cli_opt_status_fprint(stderr, &opt);
-			cli_opt_usage_fprint(stderr, PROGRAM_NAME, NULL, common_opts);
+			cli_opt_usage_fprint(stderr, PROGRAM_NAME, NULL, cli_common_opts);
 			error = 129;
 			goto done;
+		}
+
+		/*
+		 * When we see a command, stop parsing and capture the
+		 * remaining arguments as args for the command itself.
+		 */
+		if (command) {
+			args = &argv[optparser.idx];
+			args_len = (int)(argc - optparser.idx);
+			break;
 		}
 	}
 
@@ -43,6 +67,21 @@ int main(int argc, char **argv)
 		printf("%s version %s\n", PROGRAM_NAME, LIBGIT2_VERSION);
 		goto done;
 	}
+
+	/* If there was no command, we want to invoke "help" */
+	if (!command || show_help) {
+		cli_opt_usage_fprint(stdout, PROGRAM_NAME, NULL, cli_common_opts);
+		goto done;
+	}
+
+	if ((cmd = cli_cmd_spec_byname(command)) == NULL) {
+		fprintf(stderr, "%s: '%s' is not a %s command. See '%s help'.\n",
+			PROGRAM_NAME, command, PROGRAM_NAME, PROGRAM_NAME);
+		error = 1;
+		goto done;
+	}
+
+	error = cmd->fn(args_len, args);
 
 done:
 	git_libgit2_shutdown();
