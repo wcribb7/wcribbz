19d5d6ce16d75f778b93515c503c7bdf37b9fee5
Edward Thomson <ethomson@edwardthomson.com>
2020-05-03 23:13:28 +0100

    cli: infrastructure for a cli project
    Introduce a command-line interface for libgit2.  The goal is for it to
    be git-compatible.
    
    1. The libgit2 developers can more easily dogfood libgit2 to find bugs,
       and performance issues.
    
    2. There is growing usage of libgit2's examples as a client; libgit2's
       examples should be exactly that - simple code samples that illustrate
       libgit2's usage.  This satisfies that need directly.
    
    3. By producing a client ourselves, we can better understand the needs
       of client creators, possibly producing a shared "middleware" for
       commonly-used pieces of client functionality like interacting with
       external tools.
    
    4. Since git is the reference implementation, we may be able to benefit
       from git's unit tests, running their test suite against our CLI to
       ensure correct behavior.
    
    This commit introduces a simple infrastructure for the CLI.
    
    The CLI is currently links libgit2 statically; this is because the
    utility layer is required for libgit2 _but_ shares the error state
    handling with libgit2 itself.  There's no obviously good solution
    here without introducing annoying indirection or more complexity.
    Until we can untangle that dependency, this is a good step forward.
    
    In the meantime, we link the libgit2 object files, but we do not include
    the (private) libgit2 headers.  This constrains the CLI to the public
    libgit2 interfaces.

---
+++ src/cli/main.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+
+#include <stdio.h>
+#include <git2.h>
+#include "cli.h"
+
+static int show_version = 0;
+
+static const cli_opt_spec common_opts[] = {
+	{ CLI_OPT_SWITCH, "version",   0, &show_version, 1, NULL,      "display the version" },
+	{ 0 }
+};
+
+int main(int argc, char **argv)
+{
+	cli_opt_parser optparser;
+	cli_opt opt;
+	int error = 0;
+
+	if (git_libgit2_init() < 0) {
+		fprintf(stderr, "error: failed to initialize libgit2\n");
+		exit(1);
+	}
+
+	cli_opt_parser_init(&optparser, common_opts, argv + 1, argc - 1);
+
+	/* Parse the top-level (common) options and command information */
+	while (cli_opt_parser_next(&opt, &optparser)) {
+		if (!opt.spec) {
+			cli_opt_status_fprint(stderr, &opt);
+			cli_opt_usage_fprint(stderr, PROGRAM_NAME, common_opts);
+			error = 129;
+			goto done;
+		}
+	}
+
+	if (show_version) {
+		printf("%s version %s\n", PROGRAM_NAME, LIBGIT2_VERSION);
+		goto done;
+	}
+
+done:
+	git_libgit2_shutdown();
+	return error;
+}
