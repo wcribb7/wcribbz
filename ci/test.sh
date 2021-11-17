678a6e9098cd4e636aad7504a4c4838847ede201
Edward Thomson <ethomson@edwardthomson.com>
2021-11-17 14:31:29 -0500

    cmake: refactor `add_clar_test` into separate module

--- ci/test.sh
+++ ci/test.sh
@@ -155,10 +155,6 @@ fi
 
 # Run the tests that do not require network connectivity.
 
-if [ -z "$SKIP_UTILITY_TESTS" ]; then
-	run_test util
-fi
-
 if [ -z "$SKIP_OFFLINE_TESTS" ]; then
 	echo ""
 	echo "##############################################################################"
