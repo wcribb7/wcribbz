51c074470ccd107da1ce3f49ab0c87fbc38e2300
Edward Thomson <ethomson@edwardthomson.com>
2023-05-17 17:44:00 +0100

    fixup! Introduce git_process class that invokes processes

--- tests/util/process/win32.c
+++ tests/util/process/win32.c
@@ -3,13 +3,10 @@
 #include "vector.h"
 
 #ifdef GIT_WIN32
-static git_strarray input;
 static git_str result;
 
 # define assert_cmdline(expected, given) do { \
-		input.count = ARRAY_SIZE(given); \
-		input.strings = given; \
-		cl_git_pass(git_process__cmdline(&result, &input)); \
+		cl_git_pass(git_process__cmdline(&result, given, ARRAY_SIZE(given))); \
 		cl_assert_equal_s(expected, result.ptr); \
 		git_str_dispose(&result); \
 	} while(0)
