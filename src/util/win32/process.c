zmcclernon/clickup

process: test SIGTERM detection
We can't reliably detect SIGPIPE on close because of platform
differences. Track `pid` and send `SIGTERM` to a function and ensure
that we can detect it.

src/util/win32/process.c
@@ -288,6 +288,19 @@ on_error:
 	return -1;
 }
 
+int git_process_id(p_pid_t *out, git_process *process)
+{
+	GIT_ASSERT(out && process);
+
+	if (!process->process_information.dwProcessId) {
+		git_error_set(GIT_ERROR_INVALID, "process not running");
+		return -1;
+	}
+
+	*out = process->process_information.dwProcessId;
+	return 0;
+}
+
 ssize_t git_process_read(git_process *process, void *buf, size_t count)
 {
 	DWORD ret;

wcribb7/wcribbz

src/util/win32/process.c
@@ -288,6 +288,19 @@ on_error:
 	return -1;
 }
 
+int git_process_id(p_pid_t *out, git_process *process)
+{
+	GIT_ASSERT(out && process);
+
+	if (!process->process_info.dwProcessId) {
+		git_error_set(GIT_ERROR_INVALID, "process not running");
+		return -1;
+	}
+
+	*out = process->process_info.dwProcessId;
+	return 0;
+}
+
 ssize_t git_process_read(git_process *process, void *buf, size_t count)
 {
 	DWORD ret;